181df67f590798aa2a084e7d9b11173839e58458
Vinicius-Marcondes <viniciusmarc_@hotmail.com>
2020-03-16 17:42:33 -0300

    redis, worker and queue added

--- email-server/app/app.sh
+++ email-server/app/app.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-pip install bottle==0.12.18 psycopg2
+pip install bottle==0.12.18 psycopg2 redis==3.4.1
 python -u sender.py
--- email-server/app/sender.py
+++ email-server/app/sender.py
@@ -1,29 +1,38 @@
 import psycopg2
-from bottle import route, run, request
-
-DSN = 'dbname=email_sender user=postgres password=password host=postgres-db'
-SQL = 'INSERT INTO emails (subject, message) VALUES (%s, %s)'
-
-def register_message(subject, message):
-    conn = psycopg2.connect(DSN)
-    cur = conn.cursor()
-    cur.execute(SQL, (subject, message))
-    conn.commit()
-    cur.close()
-    conn.close()
-
-    print("The message was registered!")
+import redis
+import json
+from bottle import Bottle, request
 
 
-@route('/', method='POST')
-def send():
-    subject = request.forms.get('subject')
-    message = request.forms.get('message')
 
-    register_message(subject, message)
-    return 'Queued message! Subject: {} Message: {}'.format(
-        subject, message
-    )
+class Sender(Bottle):
+    def __init__(self):
+        super().__init__()
+        self.route('/', method='POST', callback=self.send)
+        self.queue = redis.StrictRedis(host='queue', port=6379, db=0)
+        DSN = 'dbname=email_sender user=postgres password=password host=postgres-db'
+        self.conn = psycopg2.connect(DSN)
+
+    def register_message(self,subject, message):
+        SQL = 'INSERT INTO emails (subject, message) VALUES (%s, %s)'
+        cur = self.conn.cursor()
+        cur.execute(SQL, (subject, message))
+        self.conn.commit()
+        cur.close()
+
+        msg = {'subject': subject, 'message': message}
+        self.queue.rpush('sender', json.dump(msg))
+        print("The message was registered!")
+
+    def send(self):
+        subject = request.forms.get('subject')
+        message = request.forms.get('message')
+
+        self.register_message(subject, message)
+        return 'Queued message! Subject: {} Message: {}'.format(
+            subject, message
+        )
 
 if __name__ == '__main__':
-    run(host='0.0.0.0', port=8080, debug=True)
+    sender = Sender()
+    sender.run(host='0.0.0.0', port=8080, debug=True)
